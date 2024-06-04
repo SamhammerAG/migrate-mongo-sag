5eea00aa148af7bd7391afe246a3c01e75c83b54
Roland Schmitt <rschmitt@samhammer.de>
2024-06-04 17:32:07 +0200

    MET-3283 add execute support to package https://docs.npmjs.com/cli/v10/configuring-npm/package-json#bin

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,4 @@
-//https://www.mongodb.com/docs/drivers/node/current/compatibility/
-//mongodb@6 breaks db.addUser, mongodb@4 still compatible with mongodb-server@6 & migrate-mongo@11
-
-//https://github.com/seppevs/migrate-mongo?tab=readme-ov-file#api-usage
-//use of api allows us to add new commdands (e.g. dropDatabase) and pre-processing (e.g. merge script direcitories)
-
-//https://www.npmjs.com/package/@samhammer/vault-client-sag
-//we may include this directly to replace placeholders or add support for config post-processing
-
-//https://www.npmjs.com/package/dotenv#%EF%B8%8F-usage
-//we my included this to configure our secrets in .env files and automatically put them to process.env
-
-//https://www.npmjs.com/package/commander
-//we use that to support actions before cli commands (same lib as migrate-mongo uses)
-//we may add options for brand, environment (and instance app/admin) to make it unnecessary to have multiple config profiles
-
-//https://github.com/seppevs/migrate-mongo/blob/master/lib/env/config.js#L66C36-L66C48
-//we can use some logic to load some pre/post-processing scripts that are defined externally (see sample above)
+#! /usr/bin/env node
 
 import { create, database, config, up, down, status } from "migrate-mongo";
 import { Command } from "commander";
@@ -24,7 +7,7 @@ import { loadEnv } from "./env";
 
 const program = new Command();
 
-program.name(pkgjson.name).description("CLI to migrate  mongodb").version(pkgjson.version);
+program.name("migrate-mongo").description("CLI to migrate  mongodb").version(pkgjson.version);
 
 program.hook("preSubcommand", async () => {
     await loadEnv();
