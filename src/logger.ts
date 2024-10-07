d60960c7fd0e13e42532fd01e0e5a4c9697bc8d1
Khoa <khoa.nguyen@samhammer.de>
2024-10-07 11:28:20 +0700

    ASMO-6225 update default log values

--- src/logger.ts
+++ src/logger.ts
@@ -2,13 +2,16 @@ import ecsFormat from "@elastic/ecs-winston-format";
 import winston from "winston";
 
 export function initLogger() {
+    const defaultLogLevel = "info";
+    const defaultLogFilePath = "logs/log.json";
+
     const logger = winston.createLogger({
-        level: process.env.Logger_LogLevel,
+        level: process.env.Logger_LogLevel || defaultLogLevel,
         format: ecsFormat({ convertReqRes: true }),
         transports: [
             new winston.transports.Console(),
             new winston.transports.File({
-                filename: process.env.Logger_LogFile,
+                filename: process.env.Logger_LogFile || defaultLogFilePath,
                 level: process.env.Logger_LogLevel
             })
         ]
