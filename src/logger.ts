15c69cdf6d959e852131b3d4a9653343c4711d9a
Khoa <khoa.nguyen@samhammer.de>
2024-10-15 09:36:52 +0700

    ASMO-6225 Update fields for logs

--- src/logger.ts
+++ src/logger.ts
@@ -1,13 +1,24 @@
 import ecsFormat from "@elastic/ecs-winston-format";
-import winston from "winston";
+import winston, { format } from "winston";
 
 export function initLogger() {
     const consoleLog = new winston.transports.Console();
     const fileLog = new winston.transports.File({ filename: process.env.Logger_LogFile });
+    const ecsJsonFormat = ecsFormat();
+    const fieldsFormat = format((info) => {
+        return {
+            ...info,
+            fields: {
+                Brand: process.env.Brand,
+                Environment: process.env.Environment,
+                Branch: process.env.Branch
+            }
+        };
+    })();
 
     const logger = winston.createLogger({
         level: process.env.Logger_LogLevel,
-        format: ecsFormat({ convertReqRes: true }),
+        format: format.combine(fieldsFormat, ecsJsonFormat),
         transports: [consoleLog, fileLog]
     });
 
